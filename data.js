30904c08617ea0f3dbc10728254847f6dc729467
joaofranciscosousa <jfsousacarvalho@gmail.com>
2022-01-30 15:02:59 -0300

    final version

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        url: "https://v16-webapp.tiktok.com/45a1d8647d0de062ca2e09b5bcf97b20/61d66bba/video/tos/useast2a/tos-useast2a-ve-0068c003/a9f0138bb72847a5b851e4793463ab95/?a=1988&br=2732&bt=1366&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_Fxrkag3-I&l=20220105220959010223120096046106B8&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amdmbXh0cjU0NDMzNzczM0ApOTtmPGlpZTw8NzllODs5PGdhbXFpazE1L2dgLS1kMTZzcy40XzQyMWBfMi9iMi9hXi86Yw%3D%3D&vl=&vr=",
+        channel: "Channel by super props",
+        description: "description by super props",
+        song: "song by super props",
+        likes: "123",
+        messages: "200",
+        shares: "12",
+    },
+    {
+        url: "https://v16-webapp.tiktok.com/45a1d8647d0de062ca2e09b5bcf97b20/61d66bba/video/tos/useast2a/tos-useast2a-ve-0068c003/a9f0138bb72847a5b851e4793463ab95/?a=1988&br=2732&bt=1366&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_Fxrkag3-I&l=20220105220959010223120096046106B8&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amdmbXh0cjU0NDMzNzczM0ApOTtmPGlpZTw8NzllODs5PGdhbXFpazE1L2dgLS1kMTZzcy40XzQyMWBfMi9iMi9hXi86Yw%3D%3D&vl=&vr=",
+        channel: "Channel by super props",
+        description: "description by super props",
+        song: "song by super props",
+        likes: "123",
+        messages: "200",
+        shares: "12",
+    }
+]
+
+export default data;
---
+++ server.js
@@ -0,0 +1,57 @@
+import express from "express"
+import mongoose from "mongoose"
+import Data from './data.js'
+import Videos from './dbModel.js'
+
+//app config test
+const app = express()
+const port = process.env.PORT || 9000
+
+//middlwares 
+app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','*'),
+    next()
+})
+
+//DB config
+const connection_url= 'mongodb+srv://admin:CBboURitNp7ckUJJ@cluster0.p6ave.mongodb.net/tiktok?retryWrites=true&w=majority';
+
+mongoose.connect(connection_url,{
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+//api endpoints
+app.get("/", (req, res) => res.status(200).send("Hello Word 1° try!!!"))
+
+app.get("/v1/posts", (req, res) => res.status(200).send(Data))
+
+app.get("/v2/posts", (req, res) => {
+    Videos.find((err, data) => {
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.post("/v2/posts", (req, res) => {
+    //POST request is to ADD DATA to the database
+    //It will let us to ADD a video DOCUMENT to the videos COLLECTIONS
+    const dbVideos = req.body
+
+    Videos.create(dbVideos, (err, data) => {
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    })
+})
+
+//listen
+app.listen(port, () => console.log(`listen on localhost: ${port}`))
